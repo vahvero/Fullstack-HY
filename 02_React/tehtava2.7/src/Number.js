a4c5c4fa692606924583454b4587d78ac7285c83
vahvero <otso.brummer@gmail.com>
2018-10-22 17:35:48 +0300

    minor dev from id -> _id

--- 02_React/tehtava2.7/src/Number.js
+++ 02_React/tehtava2.7/src/Number.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Number = ({person, onClickMethod}) => {
     return(
         <div>
-            <p>{person.name} : {person.number}</p><button onClick={() => onClickMethod(person.id, person.name)}>Poista</button>
+            <p>{person.name} : {person.number}</p><button onClick={() => onClickMethod(person._id, person.name)}>Poista</button>
         </div>
     )
 }
